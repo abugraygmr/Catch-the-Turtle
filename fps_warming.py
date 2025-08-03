e456130d741210b76bc478fa89551b7fc7caaac4
Ahmet Buğra Yağmur <abugrayagmur1@gmail.com>
2025-08-04 02:05:00 +0300

    second commit

--- fps_warming.py
+++ fps_warming.py
@@ -8,13 +8,18 @@ turtle_screen.bgcolor("black")
 turtle_screen.title("FPS Warm-Up Game")
 turtle_screen.tracer(False)
 
+#turtle
+turtle.shape("circle")
+turtle.shapesize(3,3,3)
+turtle.color("yellow")
+
 #score
-score_heading = turtle.Turtle()
-score_heading.hideturtle()
-score_heading.penup()
-score_heading.setpos(-67,225)
-score_heading.color("white")
-score_heading.write("SCORE :", font=("Comic Sans", 20, "normal"))
+score_text = turtle.Turtle()
+score_text.hideturtle()
+score_text.penup()
+score_text.setpos(-67,225)
+score_text.color("white")
+score_text.write("SCORE :", font=("Comic Sans", 20, "normal"))
 
 score = 0
 
@@ -27,24 +32,19 @@ score_point.write(str(score), font=("Comic Sans", 20, "normal"))
 
 def score_raise(x,y):
     global score
-
     score += 1
     score_point.clear()
     score_point.write(str(score), font=("Comic Sans", 20, "normal"))
 
 turtle.onrelease(score_raise,1)
 
+#moving the turtle
 def chn_loc(x,y):
     a = random.randint(-300, 300)
     b = random.randint(-300, 300)
     turtle.teleport(a,b)
 
 turtle.onclick(chn_loc,1)
-
-#turtle
-turtle.shape("turtle")
-turtle.shapesize(3,3,3)
-turtle.color("yellow")
 
 #timer
 time_text = turtle.Turtle()
@@ -54,14 +54,14 @@ time_text.hideturtle()
 time_text.setpos(-50,265)
 time_text.write("TİME: ",font=("Comic Sans", 20, "normal"))
 
-timer_text = turtle.Turtle()
-timer_text.color("red")
-timer_text.hideturtle()
-timer_text.setpos(30,265)
+time_number = turtle.Turtle()
+time_number.color("red")
+time_number.hideturtle()
+time_number.setpos(30,265)
 start = time.time()
 while time.time() - start < 20:
-    timer_text.clear()
-    timer_text.write(int(20 - (time.time() - start)), font=("Comic Sans", 20, "normal"))
+    time_number.clear()
+    time_number.write(int(20 - (time.time() - start)), font=("Comic Sans", 20, "normal"))
     turtle_screen.update()
     continue
 
@@ -70,18 +70,13 @@ def game_over():
     game_ovr_text.hideturtle()
     game_ovr_text.color("red")
     game_ovr_text.penup()
-
-    game_ovr_text.teleport(-125, 263)
-
-    timer_text.clear()
+    game_ovr_text.setpos(-125, 263)
+    time_number.clear()
     time_text.clear()
-
-
     game_ovr_text.write("GAME OVER !", font=("Comic Sans", 30, "bold"))
-
     turtle.hideturtle()
 
-turtle.ontimer(game_over, 20)
+turtle.ontimer(game_over)
 
 turtle_screen.tracer(True)
 
